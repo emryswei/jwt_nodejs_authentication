381f4548573dedfccadf9813de6bba840ec021c1
yao chengwei <tracyyaocw@gmail.com>
2021-11-08 18:13:17 +0800

    add auth and start using mongodb

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(port, (req,res)=>console.log('server start'))
+dotenv.config();
+
+// connect to the  mongodb
+mongoose.connect(process.env.MONGODB_CONNECT, () =>
+  console.log("connect to mongodb")
+);
+
+// import routers
+const authRoute = require("./routes/auth");
+
+// router middlewares
+app.use("/api/user", authRoute);
+
+app.listen(port, (req, res) => console.log("local server start"));
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.post("/Register", (req, res) => {
+  res.send("Auth responsed");
+});
+
+module.exports = router;
