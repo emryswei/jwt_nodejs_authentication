bed9aea765e1f8cbad4994038682b3a681ce6efe
yao chengwei <tracyyaocw@gmail.com>
2021-11-09 18:30:00 +0800

    add schema using Joi for validation

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const app = express();
 
 // import routers
 const authRoute = require("./routes/auth");
+
 dotenv.config();
 
 // connect to the  mongodb
 mongoose.connect(process.env.MONGODB_CONNECT, () =>
-  console.log("connect to mongodb")
+  console.log("sucessfully connect to mongodb")
 );
 
 // middleware
+// body-parser 是 Express 經常使用的中介軟體，用於解析請求的資料(body)
+// 流程：user => post data => node.js => Express => body-parse =>middlewaves,最後從middlewaves返回結果到node.js => user
+// express.json()解析request中body部分的json內容，return Object
+// header必須設置為‘content-type: application/json’
 app.use(express.json());
 
-// router middlewares, 第二個參數是callback function
+// router middlewares, .use()第二個參數是callback function
 app.use("/api/user", authRoute);
 
 app.listen(port, (req, res) => console.log("local server start"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+// validation using joi
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+});
+
 router.post("/Register", async (req, res) => {
+  // validate the input information
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     password: req.body.password,
