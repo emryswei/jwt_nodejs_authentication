5775f7a386fcb4298832f6ea4db925d7416c17ba
yao chengwei <tracyyaocw@gmail.com>
2021-11-09 12:33:38 +0800

    add userschema

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+const express = require("express");
+const app = express();
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ mongoose.connect(process.env.MONGODB_CONNECT, () =>
 // import routers
 const authRoute = require("./routes/auth");
 
-// router middlewares
+// router middlewares, 第二個參數是callback function
 app.use("/api/user", authRoute);
 
 app.listen(port, (req, res) => console.log("local server start"));
